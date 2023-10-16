38b18334bf78be25856e39a3ed82481a42ee6204
MerchantBob <122754743+MerchantBob@users.noreply.github.com>
2023-10-16 19:29:39 +0400

    some improved textures and tooltip for ice cream jars

--- src/main/java/com/ghasto/create_scoops_and_shakes/block/ice_cream_jar/IceCreamJarItem.java
+++ src/main/java/com/ghasto/create_scoops_and_shakes/block/ice_cream_jar/IceCreamJarItem.java
@@ -1,15 +1,19 @@
 package com.ghasto.create_scoops_and_shakes.block.ice_cream_jar;
 
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.ghasto.create_scoops_and_shakes.ModBlockEntities;
+import com.ghasto.create_scoops_and_shakes.ModBlocks;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class IceCreamJarItem extends BlockItem {
 	public IceCreamJarItem(Block block, Properties properties) {
@@ -18,5 +22,10 @@ public class IceCreamJarItem extends BlockItem {
 
 	@Override
 	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+		IceCreamJarBlockEntity be = new IceCreamJarBlockEntity(ModBlockEntities.ICE_CREAM_JAR.get(), BlockPos.ZERO, ModBlocks.ICE_CREAM_JAR.getDefaultState());
+		if(stack.getTag() == null)
+			return;
+		be.readClient(stack.getTag());
+		tooltipComponents.add(be.fluidTankBehaviour.getPrimaryTank().getRenderedFluid().getDisplayName());
 	}
 }
